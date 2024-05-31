e52545711cbdd6f309b22c4fbb7f174521fd93e7
Khushi-M-0521 <khushim0521@gmail.com>
2024-05-31 12:12:11 +0530

    modifications

--- colorCube.c
+++ colorCube.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <GL/glut.h>
- GLfloat vertices[][3] = {{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},{1.0,1.0,-1.0}, {-1.0,1.0,-1.0}, {-1.0,-1.0,1.0},{1.0,-1.0,1.0}, {1.0,1.0,1.0}, {-1.0,1.0,1.0}};
-GLfloat normals[][3] = {{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},   {1.0,1.0,-1.0}, {-1.0,1.0,-1.0}, {-1.0,-1.0,1.0} ,{1.0,-1.0,1.0}, {1.0,1.0,1.0}, {-1.0,1.0,1.0}};
+ GLfloat vertices[][3] = {{-10.0,-10.0,-10.0},{10.0,-10.0,-10.0},{10.0,10.0,-10.0}, {-10.0,10.0,-10.0}, {-10.0,-10.0,10.0},{10.0,-10.0,10.0}, {10.0,10.0,10.0}, {-10.0,10.0,10.0}};
+GLfloat normals[][3] = {{-10.0,-10.0,-10.0},{10.0,-10.0,-10.0},   {10.0,10.0,-10.0}, {-10.0,10.0,-10.0}, {-10.0,-10.0,10.0} ,{10.0,-10.0,10.0}, {10.0,10.0,10.0}, {-10.0,10.0,10.0}};
 GLfloat colors[][3] = {{0.0,0.0,0.0},{1.0,0.0,0.0}, {1.0,1.0,0.0}, {0.0,1.0,0.0}, 	{0.0,0.0,1.0},{1.0,0.0,1.0}, {1.0,1.0,1.0}, {0.0,1.0,1.0}};
 static GLfloat theta[] = {0.0,0.0,0.0};
 static GLint axis = 2;
@@ -34,11 +34,19 @@ void colorcube(void)
         polygon(4,5,6,7);
         polygon(0,1,5,4);
 }
+void drawText(int x,int y,char* s){
+	int i=0;
+	glColor3f(1,1,1);
+	glRasterPos2i(x,y);
+	for(i=0;s[i]!='\0';i++)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10,s[i]);
+}
 void display(void)
-{/* display callback, clear frame buffer and z buffer,
-   rotate cube and draw, swap buffers */
+{/* display callback, clear frame buffer and z buffer, rotate cube and draw, swap buffers */
  	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
+        drawText(-18,15,"Name");
+        drawText(15,15,"usn");
         glRotatef(theta[0], 1.0, 0.0, 0.0);
         glRotatef(theta[1], 0.0, 1.0, 0.0);
         glRotatef(theta[2], 0.0, 0.0, 1.0);
@@ -48,10 +56,10 @@ void display(void)
 }
 void spinCube()
 {/* Idle callback, spin cube 2 degrees about selected axis*/ 
-	theta[axis] += 0.1;
+	theta[axis] += 0.05;
         if( theta[axis] > 360.0 ) theta[axis] -= 360.0;
-        	/* display(); */
-        	glutPostRedisplay();
+        /* display(); */
+        glutPostRedisplay();
 }
 void mouse(int btn, int state, int x, int y)
 {/* mouse callback, selects an axis about which to rotate */  
@@ -65,9 +73,9 @@ void myReshape(int w, int h)
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         if (w <= h)
-        	glOrtho(-2.0, 2.0, -2.0 * (GLfloat) h / (GLfloat) w, 2.0 * (GLfloat) h / (GLfloat) w, -10.0, 10.0);
+        	glOrtho(-20.0, 20.0, -20.0 * (GLfloat) h / (GLfloat) w, 20.0 * (GLfloat) h / (GLfloat) w, -100.0, 100.0);
         else
-        	glOrtho(-2.0 * (GLfloat) w / (GLfloat) h, 2.0 * (GLfloat) w / (GLfloat) h, -2.0, 2.0, -10.0, 10.0);
+        	glOrtho(-20.0 * (GLfloat) w / (GLfloat) h, 20.0 * (GLfloat) w / (GLfloat) h, -20.0, 20.0, -100.0, 100.0);
         glMatrixMode(GL_MODELVIEW);
 }
 void main(int argc, char **argv)
--- transformation2D.c
+++ transformation2D.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<math.h>
 #include<GL/glut.h>
-GLfloat Triangle[3][3]={{100.0,250.0,175.0},{100.0,100.0,300.0},{1.0,1.0,1.0}};
+GLfloat Triangle[3][3]={{150.0,300.0,225.0},{150.0,150.0,350.0},{1.0,1.0,1.0}};
 GLfloat rotatement[3][3]={{0},{0},{0}};
 GLfloat Result[3][3]={{0},{0},{0}};
 GLfloat m=0;
@@ -10,20 +10,30 @@ GLfloat n=0;
 float theta;
 void triangle()
 {
-	glColor3f(1.0,0.0,0.0);
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(Triangle[0][0],Triangle[1][0]);
 	glVertex2f(Triangle[0][1],Triangle[1][1]);
 	glVertex2f(Triangle[0][2],Triangle[1][2]);
 	glEnd();
 }
+void drawText(int x,int y,char* s){
+	int i;
+	glColor3f(0,1,0);
+	glRasterPos2i(x,y);
+	for(i=0;s[i]!='\0';i++)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10,s[i]);
+}
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	drawText(380,480,"Name");
+	drawText(380,465,"usn");
+	glColor3f(1,0,0);
 	triangle();
 	glTranslatef(m,n,0);
 	glRotatef(theta,0,0,1);
 	glTranslatef(-m,-n,0);
+	glColor3f(0,0,1);
 	triangle();
 	glFlush();
 }
@@ -34,21 +44,21 @@ void myinit()
 	glPointSize(1);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0,449,0,499);
+	gluOrtho2D(0,500,0,500);
 }
 int main(int argc,char **argv)
 {
 	int ch;
-	printf("enter choice\n 1:Rotation about origin \n 2:rotation about a fixed point\n");
+	printf("Enter choice\n 1:Rotation about origin \n 2:rotation about a fixed point\n:");
 	scanf("%d",&ch);
 	switch(ch)
 	{
-		case 1:printf("enter the rotation angle in degree:");
+		case 1:printf("Enter the rotation angle in degree:");
 			scanf("%f",&theta);
 			break;
-		case 2:printf("enter the fixed point:");
+		case 2:printf("Enter the fixed point:");
 			scanf("%f%f",&m,&n);
-			printf("enter the rotation angle:");
+			printf("Enter the rotation angle:");
 			scanf("%f",&theta);
 			break;
 	}
